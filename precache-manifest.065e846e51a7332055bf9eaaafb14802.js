9c3b2ec05d7954a1f538f280b14cb9fc1b339d75
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-09-23 19:55:02 +0100

    Updates

---
+++ precache-manifest.065e846e51a7332055bf9eaaafb14802.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9221a516b8afe544feeaeeac3594adbe",
+    "url": "/path-finder/index.html"
+  },
+  {
+    "revision": "5b9643272fec5c80d1f1",
+    "url": "/path-finder/static/css/main.68080c8b.chunk.css"
+  },
+  {
+    "revision": "f35de003c702f3405efe",
+    "url": "/path-finder/static/js/2.02cc0741.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/path-finder/static/js/2.02cc0741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b9643272fec5c80d1f1",
+    "url": "/path-finder/static/js/main.e4e67600.chunk.js"
+  },
+  {
+    "revision": "0e2b03c3719913095fdf",
+    "url": "/path-finder/static/js/runtime-main.1c1d10e2.js"
+  }
+]);
